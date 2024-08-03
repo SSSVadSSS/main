b6b259b529a6bd020dfcf32462b35e90bdad4fb9
SSSVadSSS <systemprofi@mail.ru>
2024-08-03 10:17:44 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+homeworks_count = 12
+hourses_count = 1.5
+course_name = 'Pyton'
+print("Курс: "+course_name + ",","всего задач:",homeworks_count,",",'затрачено часов:', hourses_count,",",
+      'среднее время выполнения',hourses_count/homeworks_count, 'часа.')
