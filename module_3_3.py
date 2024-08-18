3f9c984384d2ceca9582ff3c4e9d8a58ca4f1db7
SSSVadSSS <systemprofi@mail.ru>
2024-08-18 14:54:32 +0300

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -3,11 +3,9 @@ def print_params(a=1, b='строка', c=True):
 
 
 values_list = [1, [1, 2], True]
-values_dict = {'a': 1, 'b': 'urbab', 'c': False}
+values_dict = {'a': 1, 'b': 'urban', 'c': False}
 
 # print_params(**values_dict)
 
-values_list_2 = [54.32, 'Строка' ]
+values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
-
-
